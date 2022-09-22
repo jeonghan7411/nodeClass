b581758de693881fe9cce7438295ab5d92f29c2f
jeonghan7411 <aufkrh@naver.com>
2022-09-22 10:51:36 +0900

    NodeJS 2교시

---
+++ 0-crud/1-insert/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+
+const app = express();
+const PORT = 3000;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "login",
+  password: "1234",
+  database: "login",
+});
+
+//db.connect();
+db.connect((err) => {
+  if (!err) {
+    console.log("Mysql Success!");
+  } else {
+    console.log(err);
+  }
+});
+
+app.set("view engine", "ejs"); //ejs middleware -views 안에 있는 파일을 읽어 오기
+app.use(express.urlencoded({ extended: false }));
+
+let title = "Member Management System";
+app.get("/addUser", (req, res) => {
+  res.render("insert", { title, subTitle: "> Insert" });
+});
+
+app.post("/addUser", (req, res) => {
+  let sql = "INSERT INTO  register VALUES(NULL, ?,?,nodw());";
+  // sql 문을 실행 (sql,[?값을 순서대로 넣어주면 됨 ?가 없으면 생략],콜백)
+  //? 가 있으면 []
+  db.query(sql, [req.body.username, req.body.passwd], (err) => {});
+});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
---
+++ 0-crud/1-insert/user.sql
@@ -0,0 +1,10 @@
+-- 데이터베이스 선택
+use login; 
+
+CREATE TABLE register(
+  regID int not null auto_increment primary key,
+  username  varchar(10) not null,
+  passwd  varchar(20) not null, 
+  regdate date
+);
+
